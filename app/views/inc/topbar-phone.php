455d412033daf3d3628defbca368716bb779b4d8
amir-fatnassi <you@example.com>
2020-03-02 12:06:51 +0100

    header

--- app/views/inc/topbar-phone.php
+++ app/views/inc/topbar-phone.php
@@ -1,47 +1,94 @@
-<div class="topbar-mobile  hidden-lg hidden-md">
-    <div class="active-mobile pull-left">
-        <button data-toggle="offcanvas" class="btn btn-offcanvas btn-toggle-canvas offcanvas" type="button">
-            <i class="fa fa-bars"></i>
-        </button>
-    </div>
-    <div class="topbar-inner pull-left">
-        <div class="active-mobile search-popup pull-left">
-            <span class="fa fa-search"></span>
-            <div class="active-content">
-                <form method="get" class="input-group search-category" action="http://demo2.themelexus.com/medicare/">
-                    <div class="input-group-addon search-category-container">
-                        <div class="select">
-                            <select name="product_cat" id="product_cat" class="dropdown_product_cat">
-                                <option value="" selected="selected">Select a category</option>
-                                <option class="level-0" value="best-sellers">Best sellers&nbsp;&nbsp;(11)</option>
-                                <option class="level-0" value="new-arrivals">New arrivals&nbsp;&nbsp;(12)</option>
-                                <option class="level-0" value="pharmacies">Pharmaceuticals&nbsp;&nbsp;(23)</option>
-                                <option class="level-1" value="drug">&nbsp;&nbsp;&nbsp;Drug Testing&nbsp;&nbsp;(4)</option>
-                                <option class="level-1" value="medication">&nbsp;&nbsp;&nbsp;Medication
-                                    Aids&nbsp;&nbsp;(22)</option>
-                                <option class="level-1" value="needles">&nbsp;&nbsp;&nbsp;Needles &amp;
-                                    Syringes&nbsp;&nbsp;(16)</option>
-                                <option class="level-1" value="supply">&nbsp;&nbsp;&nbsp;Pharmacy
-                                    Supply&nbsp;&nbsp;(5)</option>
-                                <option class="level-0" value="popular">Popular&nbsp;&nbsp;(8)</option>
-                            </select>
-                        </div>
+<div class="main-menu--logo">
+                      <a href="index.html" class="custom-logo-link" rel="home"><img width="206" height="35" src="wp-content/uploads/2018/11/cropped-logo-pediact-small.png" class="custom-logo" alt="Laboratoire PediAct" /></a>				</div>
+                      <div class="main-menu--navigation">
+                          <ul id="menu-main-menu-navigation-pages-1" class="vertical medium-horizontal menu" data-responsive-menu="accordion medium-dropdown">
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-55738 current_page_item menu-item-56356 active">
+                              <a href="index.html" aria-current="page">Accueil</a>
+                            </li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41665">
+                              <a>Nous connaÎtre</a>
+                              <ul class="menu">
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41666"><a href="notre-laboratoire/index.html">Notre Laboratoire</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41667"><a href="nous-rejoindre/index.html">Nous Rejoindre</a></li>
+                              </ul>
+                            </li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41668"><a>Nos domaines d&#8217;expertises</a>
+                              <ul class="menu">
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41669"><a href="dermatologie/index.html">Dermatologie</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41670"><a href="ferments-lactiques/index.html">Ferments Lactiques</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41671"><a href="nutrition-infantile/index.html">Nutrition Infantile</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41672"><a href="orl/index.html">ORL</a></li>
+                                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41673"><a href="pneumologie/index.html">Pneumologie</a></li>
+                              </ul>
+                            </li>
+                            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41674">
+                              <a href="#">Nos produits</a>
+                              <ul class="menu">
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41675">
+                                  <a>BioGaia®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41676"><a href="biogaia/index10eb.html?produit=gouttes">BioGaia® Gouttes</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41678"><a href="biogaia/indexca2f.html?produit=comprimes">BioGaia® Comprimés</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41679"><a href="biogaia/index4529.html?produit=vitamine">BioGaia® +  Vitamine D</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-56397"><a href="biogaia/index38a8.html?produit=minipack">BioGaia® Minipack</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41680">
+                                  <a>Capricare®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41681"><a href="capricare/index5c1c.html?produit=deuxieme-age">Capricare® 2ème âge</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41682"><a href="capricare/index6cc9.html?produit=croissance">Capricare® Croissance</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41700"><a>TipsHaler®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41701"><a href="tips-haler/indexe3ea.html?produit=nourrisson">TipsHaler® nourrisson</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41702"><a href="tips-haler/indexee26.html?produit=pediatrique">TipsHaler® pédiatrique</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41703"><a href="tips-haler/index0fee.html?produit=sans-masque">TipsHaler® sans masque</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41704"><a href="tips-haler/indexa957.html?produit=masque-adulte">TipsHaler® masque adulte</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41683">
+                                  <a>Pharyndol®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41684"><a href="pharyndol/indexe8ed.html?produit=enfant">Pharyndol® Enfant</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41685"><a href="pharyndol/index29eb.html?produit=adulte">Pharyndol® Adulte</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41688">
+                                  <a>PoxClin®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41689"><a href="poxclin/index8fc0.html?produit=varicelle">PoxClin® Varicelle</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41690"><a href="poxclin/indexe2ee.html?produit=demangeaisons">PoxClin® Démangeaisons</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41695">
+                                  <a>KidsClin®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41697"><a href="kidsclin/index48f3.html?produit=erytheme-fessier">KidsClin® Érythème fessier</a></li>
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41696"><a href="kidsclin/indexf930.html?produit=croutes-de-lait">KidsClin® Croûtes de lait</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41686">
+                                  <a>LinfoVir® Plus</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41687"><a href="linfovir-plus/index.html">LinfoVir® Plus</a></li>
+                                  </ul>
+                                </li>
+                                <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-41698">
+                                  <a>PoxKare®</a>
+                                  <ul class="menu">
+                                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41699"><a href="poxkare/index.html">PoxKare®</a></li>
+                                  </ul>
+                                </li>
+                              </ul>
+                            </li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41705"><a href="notre-blog/index.html">Notre blog</a></li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41706"><a href="nos-guides/index.html">Nos guides</a></li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-41707"><a href="nous-contacter/index.html">Nous contacter</a></li>
+                            <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-56166"><a href="ou-acheter-nos-produits/index.html">Où Acheter</a></li>
+                          </ul>
+                      	</div>
+                        <div class="main-menu--mobile-btn">
+                          <button class="" type="button" data-toggle="off-canvas">☰</button>
                     </div>
-                    <input name="s" maxlength="60" class="form-control search-category-input" type="text" size="20" placeholder="What do you need..." />
-
-                    <div class="input-group-btn">
-                        <label class="btn btn-link btn-search">
-                            <span class="title-search hidden">Search</span>
-                            <input type="submit" class="fa searchsubmit" value="&#xf002;" />
-                        </label>
-                        <input type="hidden" name="post_type" value="product" />
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="active-mobile setting-popup pull-left">
-            <span class="fa fa-user"></span>
-            <div class="active-content"></div>
-        </div>
-    </div>
-</div>
